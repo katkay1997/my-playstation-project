85769495bcfe1e561ecc309b083e4012b6656eef
kateramckay <kateradmckay@gmail.com>
2022-12-16 18:22:52 -0500

    made some edits

--- playstation-store.php
+++ playstation-store.php
@@ -14,38 +14,35 @@
 
 <body>
 
-<div class="row">
+<div class="main">
   <div class="column-12" >
     <img class="retro" src="imgs/retro-250x200.png">
   </div>
   
 <div class="banner">
-   <img src="imgs/lets-see.png" alt="clearance banner">
+   <img src="imgs/playstation-sale.png" alt="clearance banner">
   </div> 
 <hr> <!-- need to find a better set of code in the css file give a gap-->
   <div class="banner" style="background-color: #0c3e82 !important;">
-   <img src="imgs/ps-ps-text.png" alt="playstation services">
+   <img src="imgs/second-banner.png" alt="playstation services">
   </div>
 
-  <div class="column"> <!--style="height: 800px; width: 50%" -->
-   <img src="imgs/return.png" alt="returnal" >
+
+      <!--  games and gamesitems below  -->
+      <div class="games">
+    <div class="gamesitem">
+      <!--style="height: 800px; width: 50%" -->
+      <img src="imgs/fall-800.png" alt="fall guys"> <br /><br /><br />
+      <img src="imgs/calli.png">
+
+      </div>
+    <div class="gamesitem">
+      <!--style="height: 800px; width: 50%" -->
+      <img src="imgs/stray-800.png"> <br /><br /><br />
+      <img src="imgs/return.png" alt="returnal">
+    </div>
   </div>
 
-  <div class="column"> <!--style="height: 800px; width: 50%" -->
-    <img src="imgs/calli.png">
-  </div>
-</div>
- 
-
-<div class="row">
-  <div class="column"> <!--style="height: 800px; width: 50%" -->
-  <img src="imgs/fall-800.png" alt="fall guys" >
-
-  </div>
-  <div class="column"> <!--style="height: 800px; width: 50%" -->
-    <img src="imgs/stray-800.png">
-    
-  </div>
 
   <div id="footer" style="background-color:#0c3e82;">
         <p> Made with love</p>
@@ -55,5 +52,6 @@
 
 </body>
 </html>
+
 
 
